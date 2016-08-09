8d10dac3156659b0379c8e3b8b7b5a66ea3d1621
Angela Cordova <angelav98@gmail.com>
2016-08-09 18:11:12 -0500

    Creation of List model and correction of tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -56,7 +56,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
-        self.browser().quit()
+        self.browser.quit()
         self.browser = webdriver.Firefox()
 
         self.browser.get(self.live_server_url)
